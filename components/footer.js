354d8317b0ca98ad74e324d09081d9e1c2b95ead
idiz1114 <artister9814@gmail.com>
2025-10-27 13:48:52 +0900

    251027 추가수정
    - index 소개글 업데이트
    - members에 Dali people 사람들 정보 업데이트(일단 이름과 사진만 반영)

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,22 @@
 const footerTemplate = `
 <footer>
     <div class="container">
-        <p>&copy; 2025 DaLI Lab, KAIST. All rights reserved.</p>
+        <div class="footer-content">
+            <div class="footer-info">
+                <p><strong>Debiasing and Lay Informatics Lab</strong><br>
+                Room 2226, N5, KAIST<br>
+                291 Daehak-ro, Yuseong-gu, Daejeon<br>
+                Republic of Korea</p>
+            </div>
+            <div class="footer-contact">
+                <p><strong>Professor:</strong><br>
+                Prof. Jeong-Nam Kim<br>
+                Email: layinformatics@kaist.ac.kr</p>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>&copy; 2025 DaLI Lab, KAIST. All rights reserved.</p>
+        </div>
     </div>
 </footer>
 `;
